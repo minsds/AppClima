bc98dde8ce310b80a978e33b64f69fa6d4d442ae
minsds <112598677+minsds@users.noreply.github.com>
2022-09-01 15:56:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AcimaClimas from './src/componentes/AcimaClimas';
-import Cabeçalho from './src/componentes/Cabecalho';
-import Card1 from './src/componentes/Card1';
-import CardinhosAgrupados from './src/componentes/CardinhosAgrupados.js';
-import Titulo from './src/componentes/Titulo';
+import Tela1 from './src/Telas/Tela1';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.viewcabeçalho}>
-        <Cabeçalho></Cabeçalho>
-      </View>
-      <View style={styles.viewtitulo}>
-        <Titulo></Titulo>
-      </View>
-      <View style={styles.viewcard}>
-        <StatusBar style="auto" />
-        <Card1></Card1>
-      </View>
-      <div>
-      <View>
-        <AcimaClimas></AcimaClimas>
-      </View>
-      <View>
-        <CardinhosAgrupados></CardinhosAgrupados>
-      </View>
-      </div>
+    <View>
+      <Tela1></Tela1>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  viewcard: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewtitulo:{
-    alignItems: 'left',
-    justifyContent: 'left',
-  }
-});
