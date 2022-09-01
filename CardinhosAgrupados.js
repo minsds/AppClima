25d8d0422a4ec3d8074d6c11507c2ba3e3b3be78
minsds <112598677+minsds@users.noreply.github.com>
2022-09-01 10:24:33 -0300

    Update CardinhosAgrupados.js

--- CardinhosAgrupados.js
+++ CardinhosAgrupados.js
@@ -1,25 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
 import Cardinho from './Cardinho';
 
 export default function CardinhosAgrupados(props) {
   return (
-    <View style={styles.cardinhosAgrupados}>
+    <ScrollView horizontal={true} style={styles.cardinhosAgrupados}>
         <View>
-        <Cardinho hora='12:00' clima={'cloud'} textinho='Now'></Cardinho>
+        <Cardinho hora='12:00' clima={'cloud'} textinho='Now' cor={'white'}></Cardinho>
         </View>
         <View>
-        <Cardinho hora='14:00' clima={'weather-rainy'} textinho='22°'></Cardinho>
+        <Cardinho hora='14:00' clima={'weather-rainy'} textinho='22°' colortext={'black'} cor={'white'}></Cardinho>
         </View>
         <View>
-        <Cardinho hora='16:00' clima={'weather-partly-cloudy'} textinho='26°'></Cardinho>
+        <Cardinho hora='16:00' clima={'weather-partly-cloudy'} textinho='26°' cor={'white'}></Cardinho>
         </View>
         <View>
-        <Cardinho hora='18:00' clima={'weather-partly-cloudy'} textinho='25°'></Cardinho>
+        <Cardinho hora='18:00' clima={'weather-partly-cloudy'} textinho='25°' cor={'white'}></Cardinho>
         </View>
         <View>
-        <Cardinho hora='20:00' clima={'checkbox-blank-circle'} textinho='31°'></Cardinho>
+        <Cardinho hora='20:00' clima={'checkbox-blank-circle'} textinho='31°' cor={'#FFD700'}></Cardinho>
         </View>
-    </View>    
+        <View>
+        <Cardinho hora='22:00' clima={'weather-partly-cloudy'} textinho='29°' cor={'white'}></Cardinho>
+        </View>
+    </ScrollView>   
   );
 }
 
@@ -27,5 +31,5 @@ const styles = StyleSheet.create({
   cardinhosAgrupados:{
     flexDirection: 'row',
     marginRight: 20,
-  }
+  },
 })
