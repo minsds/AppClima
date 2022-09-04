55a5683b0b9073cad1475694c60f293bf38b3bbe
minsds <112598677+minsds@users.noreply.github.com>
2022-09-04 18:12:22 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Tela1 from './src/Telas/Tela1';
+import Tela2 from './src/telas/Tela2';
 
 export default function App() {
   return (
     <View>
       <Tela1></Tela1>
+      <Tela2></Tela2>
     </View>
   );
 }
