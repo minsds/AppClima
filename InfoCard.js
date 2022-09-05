631add3f34686c798921ea17e91877a6ce9a153f
minsds <112598677+minsds@users.noreply.github.com>
2022-09-05 16:17:36 -0300

    Update InfoCard.js

--- InfoCard.js
+++ InfoCard.js
@@ -1,41 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function InfoCard() {
   return (
     <View style={styles.container}>
-      <Text style={styles.cima}>WIND</Text>
-      <Text style={styles.baixo}>19.2km/j</Text>
-      <Text style={styles.cima}>FEELS LIKE</Text>
-      <Text style={styles.baixo}>25°</Text>
-      <Text style={styles.cima}>INDEX UV</Text>
-      <Text style={styles.baixo}>2</Text>
-      <Text style={styles.cima}>PRESSURE</Text>
-      <Text style={styles.baixo}>1014 mbar</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'white',
+    borderWidth: 1, paddingVertical: 18, paddingHorizontal: 45}}>
+        <MaterialCommunityIcons name={'weather-windy'} color={'white'} size={24} />
+        <View>
+          <Text style={styles.cima}>WIND</Text>
+          <Text style={styles.baixo}>19.2km/j</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'white',
+    borderWidth: 1, paddingVertical: 18,  paddingHorizontal: 45}}>
+        <MaterialCommunityIcons name={'thermometer'} color={'white'} size={24} />
+        <View>
+          <Text style={styles.cima}>FEELS</Text>
+          <Text style={styles.baixo}>25°</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'white',
+    borderWidth: 1, paddingVertical:18,  paddingHorizontal: 45}}>
+        <MaterialCommunityIcons name={'weather-sunny'} color={'white'} size={24} />
+        <View>
+          <Text style={styles.cima}>INDEX UV</Text>
+          <Text style={styles.baixo}>2</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'white',
+    borderWidth: 1, paddingVertical:18,  paddingHorizontal: 45}}>
+        <MaterialCommunityIcons name={'triangle-wave'} color={'white'} size={24} />
+        <View>
+          <Text style={styles.cima}>PRESSURE</Text>
+          <Text style={styles.baixo}>1014 mbar</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems: 'center',
-        margin:10,
-        minWidth:150,
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-    },
-    cima:{
-        color: '#D3D3D3',
-        margin: 5,
-        marginLeft: 15,
-        fontSize: 10,
-    },
-    baixo:{
-        color: 'white',
-        margin: 5,
-        marginLeft: 15,
-        fontSize: 10,
-        fontFamily: 'Trebuchet MS',
-    },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    alignItems: 'center',
+    minWidth: 150,
+ 
+  },
+  cima: {
+    color: '#D3D3D3',
+    margin: 5,
+    marginLeft: 15,
+    fontSize: 13,
+  
+  },
+  baixo: {
+    color: 'white',
+    margin: 5,
+    marginLeft: 15,
+    fontSize: 13,
+    fontFamily: 'Trebuchet MS',
+  },
 });
